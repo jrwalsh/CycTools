235d5a2c038f995ed78d62466cc6dc85d4266a6c
jrwalsh <jrwalsh@2cdb7f3e-855d-47db-aa0b-9c7ce227a9e6>
2011-05-27 19:04:22 +0000

    Main class which runs the PGDBUpdater and ToolBox methods
    git-svn-id: https://subversion.vrac.iastate.edu/Subversion/jrwalsh/svn/jrwalsh/BioCycTool@27 2cdb7f3e-855d-47db-aa0b-9c7ce227a9e6

---
+++ src/ecocycTool/Main.java
@@ -0,0 +1,66 @@
+package ecocycTool;
+
+import edu.iastate.javacyco.PtoolsErrorException;
+
+/**
+ * Main class for the ToolBox and PGDBUpdater classes.
+ * 
+ * @author Jesse Walsh
+ *
+ */
+public class Main {
+
+	/**
+	 * Main method for the ToolBox and PGDBUpdater classes.  This method initializes a connection object and calls the run() method.
+	 * 
+	 * @param args Not Used
+	 */
+	public static void main(String[] args) {
+		Long start = System.currentTimeMillis();
+		testToolBox();
+//		testPGDBUpdater();
+//		run(args);
+		Long stop = System.currentTimeMillis();
+		Long runtime = (stop - start) / 1000;
+		System.out.println("Runtime is " + runtime + " seconds.");
+	}
+
+	/**
+	 * This method initializes a ToolBox object and calls its methods.  Currently used for testing.
+	 */
+	public static void testToolBox() {
+//		ToolBox tb = new ToolBox(ToolBox.connectionStringLocal, ToolBox.defaultPort, ToolBox.organismStringCBIRC);
+		ToolBox tb = new ToolBox(ToolBox.connectionStringLocal, ToolBox.defaultPort, ToolBox.organismStringK12);
+//		ToolBox tb = new ToolBox(ToolBox.connectionStringEcoServer, ToolBox.defaultPort, ToolBox.organismStringCBIRC);
+
+		try {
+			tb.tester();
+		} catch (PtoolsErrorException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * This method initializes a PGDBUpdater object and calls its methods.  Currently used for testing.
+	 */
+	public static void testPGDBUpdater() {
+		PGDBUpdater updater = new PGDBUpdater();
+
+		try {
+			updater.tester();
+		} catch (PtoolsErrorException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * This method initializes a ToolBox object and calls its methods.  Currently used for testing.
+	 */
+	public static void run(String[] args) {
+		String connectionString = ToolBox.connectionStringLocal;
+		String organism = ToolBox.organismStringCBIRC;
+		System.out.println("Connecting to " + connectionString + ". Using organism " + organism + ".");
+		ToolBox tb = new ToolBox(connectionString, ToolBox.defaultPort, organism);
+		tb.run(args);
+	}
+}
