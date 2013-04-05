cf0d483db728245f3e82a4bbb94db0538635a967
jrwalsh <jrwalsh@2cdb7f3e-855d-47db-aa0b-9c7ce227a9e6>
2013-04-05 03:10:16 +0000

    Modified to include translating timestamps into readable time.
    git-svn-id: https://subversion.vrac.iastate.edu/Subversion/jrwalsh/svn/jrwalsh/BioCycTool@94 2cdb7f3e-855d-47db-aa0b-9c7ce227a9e6

--- src/edu/iastate/biocyctool/cycspreadsheetloader/util/CustomInterpreter.java
+++ src/edu/iastate/biocyctool/cycspreadsheetloader/util/CustomInterpreter.java
@@ -24,15 +24,51 @@ public class CustomInterpreter implements Interpretable {
 		
 		for (int rowIndex = 0; rowIndex < tb.getRowCount(); rowIndex++) {
 			String frameID = (String) tb.getValueAt(rowIndex, 0);
-//			frameUpdates.add(new SlotUpdate(frameID, "CurrentGeneModelName", (String) tb.getValueAt(rowIndex, 1)));
-			String goTerm = (String) tb.getValueAt(rowIndex, 2);
+			String goTerm = (String) tb.getValueAt(rowIndex, 1);
+			String pubMedID = (String) tb.getValueAt(rowIndex, 2);
+			String evCode = (String) tb.getValueAt(rowIndex, 3);
+			String timeStampString = (String) tb.getValueAt(rowIndex, 4);
+			String curator = (String) tb.getValueAt(rowIndex, 5);
+			
+			JavacycConnection conn = new JavacycConnection("jrwalsh.student.iastate.edu", 4444);
+			conn.selectOrganism("CORN");
+			String encodedTime = encodeTimeStampString(timeStampString, conn);
+			
 			frameUpdates.add(new SlotUpdate(frameID, "GO-TERMS", goTerm, true, true));
-//			frameUpdates.add(new SlotUpdate(frameID, "ReferenceID", (String) tb.getValueAt(rowIndex, 5)));
 			
 			ArrayList<String> annotValues = new ArrayList<String>();
-			annotValues.add("\"" +(String) tb.getValueAt(rowIndex, 4) + ":" + (String) tb.getValueAt(rowIndex, 3) + ":3501259540:" + (String) tb.getValueAt(rowIndex, 6) + "\"");
+			annotValues.add("\"" + pubMedID + ":" + evCode + ":" + encodedTime + ":" + curator + "\"");
 			frameUpdates.add(new AnnotationUpdate(frameID, "GO-TERMS", goTerm, "CITATIONS", annotValues, true, true));
 		}
 		return frameUpdates;
+	}
+	
+	// Agreed upon format is dd-mm-yyyy hh-mm-ss
+	private String encodeTimeStampString(String timeStampString, JavacycConnection conn) {
+		String encodedTime = "";
+		
+		if (timeStampString == null || timeStampString.length() == 0) {
+			return "";
+		}
+		
+		try {
+			String date = timeStampString.split(" ")[0];
+			String time = timeStampString.split(" ")[1];
+			
+			String day = date.split("-")[0];
+			String month = date.split("-")[1];
+			String year = date.split("-")[2];
+			
+			String hour = time.split("-")[0];
+			String minute = time.split("-")[1];
+			String second = time.split("-")[2];
+			
+			encodedTime = conn.encodeTimeStamp(second, minute, hour, day, month, year);
+		} catch (Exception e) {
+			System.err.println("Failed to encode timestamp " + timeStampString);
+			e.printStackTrace();
+		}
+		
+		return encodedTime;
 	}
 }
